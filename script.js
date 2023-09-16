932a7f318eca519a82fa95be27fba68aaa2b4d0d
xenionx <hank.chou16@gmail.com>
2023-09-16 10:52:17 +0800

    Library app v1.0

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const bookPages = document.querySelector('#pages');
 const hasRead = document.querySelector('#has-read');
 const submitButton = document.querySelector('.submit');
 const cardContainer = document.querySelector('.cards-container');
+const form = document.querySelector('.book-form');
 const myLibrary = [];
+const bookContainer = [];
 let bookNumber = 0;
 
 
@@ -14,6 +16,7 @@ function Book(name, author, pages, isRead){
     this.name = name;
     this.author = author;
     this.pages = pages;
+    this.isRead = isRead;
 }
 
 
@@ -21,50 +24,80 @@ function addBookToLibrary(bookName){
     bookName.bookId = bookNumber
     myLibrary.push(bookName);
     bookNumber++;
+
 }
 
-function createCard(){
+function createCard(bookId) {
     const newCard = document.createElement("div");
     const displayTitle = document.createElement('p');
     const displayAuthor = document.createElement('p');
     const displayPages = document.createElement('p');
     const readButton = document.createElement('button');
     const removeButton = document.createElement('button');
-    myLibrary.forEach((book)=>{
-        displayTitle.textContent = book.name;
-        displayAuthor.textContent = book.author;
-        displayPages.textContent = book.pages;
-        readButton.textContent = "Read";
-        readButton.setAttribute("data-book-number", book.bookId);
-        removeButton.textContent = "Remove";
-        removeButton.setAttribute("data-book-number", book.bookId)
-        removeButton.classList.add("remove-btn");
-        newCard.setAttribute("data-book-number", book.bookId);
-        
-        newCard.appendChild(displayTitle);
-        newCard.appendChild(displayAuthor);
-        newCard.appendChild(displayPages);
-        newCard.appendChild(readButton);
-        newCard.appendChild(removeButton);
-        cardContainer.appendChild(newCard);
+    const book = myLibrary.find(book => book.bookId === bookId);
 
-        removeButton.addEventListener('click', ()=>{
-                myLibrary.splice(book.bookId, 1);
-                cardContainer.removeChild(newCard);
-                bookNumber--;
-                console.table(myLibrary);
-            })
-        })
+    
+    displayTitle.textContent = book.name;
+    displayAuthor.textContent = book.author;
+    displayPages.textContent = book.pages;
+
+    readButton.setAttribute("class", book.isRead === "Read" ? "is-read" : "not-read");
+    readButton.textContent = book.isRead === "Read" ? "Read" : "Not Read";
+    readButton.setAttribute("data-book-number", book.bookId);
+    removeButton.textContent = "Remove";
+    removeButton.setAttribute("data-book-number", book.bookId);
+    removeButton.classList.add("remove-btn");
+    newCard.setAttribute("data-book-number", book.bookId);
+    
+    newCard.appendChild(displayTitle);
+    newCard.appendChild(displayAuthor);
+    newCard.appendChild(displayPages);
+    newCard.appendChild(readButton);
+    newCard.appendChild(removeButton);
+    cardContainer.appendChild(newCard);
+    
+    removeButton.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(book), 1);
+        cardContainer.removeChild(newCard);
+        bookNumber--;
+        console.table(myLibrary);
+    });
+
+    readButton.addEventListener('click', () => {
+        if(readButton.getAttribute("class") === "is-read"){
+            readButton.setAttribute("class", "not-read");
+            readButton.textContent = "Not Read";
+        }
+        else{
+            readButton.setAttribute("class", "is-read");
+            readButton.textContent = "Read";
+        }
+    })
+
+    
+
+ 
 }
 
+submitButton.addEventListener('click', () => {
+    if(!form.checkValidity()){
+        event.preventDefault();
+    }
+    else{
+        event.preventDefault();
+        formDialog.close();
+        let isRead = hasRead.checked ? "Read" : "Not Read";
+        const bookName = new Book(bookTitle.value, bookAuthor.value, bookPages.value, isRead);
+        bookTitle.value = "";
+        bookAuthor.value = "";
+        bookPages.value = "";
+        isRead = "";
+        addBookToLibrary(bookName);
+        createCard(bookName.bookId);
+        hasRead.checked ? hasRead.checked = false : null;
+    }
+})
 addButton.addEventListener('click', () => {
     formDialog.showModal();
-})
 
-submitButton.addEventListener('click', () => {
-    let isRead = hasRead.checked ? "Read" : "Not Read";
-    const bookName = new Book(bookTitle.value, bookAuthor.value, bookPages.value, isRead);
-    addBookToLibrary(bookName);
-    createCard();
 })
-
